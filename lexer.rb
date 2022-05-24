2b4d56bb473cd0e9ebe8f7663638da54571bf3cd
Ryan Chandler <ryangjchandler@gmail.com>
2022-05-25 00:41:53 +0100

    feature: parse numbers

--- lexer.rb
+++ lexer.rb
@@ -1,3 +1,11 @@
+require_relative 'util'
+
+class UnrecognisedTokenError < StandardError
+end
+
+class InvalidNumericRepresentation < StandardError
+end
+
 module TokenType
     IDENT = 'IDENT'
     SORT = 'SORT'
@@ -10,6 +18,7 @@ module TokenType
     OPEN = 'OPEN'
     STRING = 'STRING'
     NUMBER = 'NUMBER'
+    EOF = 'EOF'
 end
 
 class Token
@@ -29,15 +38,40 @@ class Lexer
         return tokens if chars.count == 0
 
         i = 0
-        while i < chars.count
+        while i < chars.count do
             char = chars[i]
             i += 1
 
             next if char.include?(' ')
 
-            tokens.append(char)
+            if alphabetic?(char)
+                word = char
+
+                while i < chars.count && alphabetic?(chars[i]) do
+                    word += chars[i]
+                    i += 1
+                end
+
+                tokens.append(Token.new(TokenType::IDENT, word))
+            elsif numeric?(char)
+                number = char
+
+                while i < chars.count && (numeric?(chars[i]) || chars[i] == '.') do
+                    if chars[i] == '.' && number.include?('.')
+                        raise InvalidNumericRepresentation.new("Numeric value already contains a period.")
+                    end
+
+                    number += chars[i]
+                    i += 1
+                end
+
+                tokens.append(Token.new(TokenType::NUMBER, number))
+            else
+                raise UnrecognisedTokenError.new("Unrecognised character #{char}.")
+            end
         end
 
+        tokens.append(Token.new(TokenType::EOF, nil))
         tokens
     end
 end
---
+++ util.rb
@@ -0,0 +1,7 @@
+def numeric?(input)
+    input =~ /[0-9]/
+end
+
+def alphabetic?(input)
+    input =~ /[A-Za-z]/
+end
